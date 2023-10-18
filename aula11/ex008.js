aef22a2f2e90d9e53f3722269f02c398698329ce
Neuris <140021259+neurisss@users.noreply.github.com>
2023-10-18 15:24:57 -0300

    criadoex008,009 e 010

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel =  60.5
+
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+console.log('Você ultapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre utlizando cinto de segurança!')
