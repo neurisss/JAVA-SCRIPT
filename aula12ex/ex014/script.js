c5fe442449804962c8fedef09bceda6904afe749
Neuris <140021259+neurisss@users.noreply.github.com>
2023-10-25 17:10:36 -0300

    ok

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -4,16 +4,16 @@ var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('imagem')
 var data =new Date()
 var hora = data.getHours()
-//var hora = 22
+//var hora = 9
 msg.innerHTML = `Agora são ${hora} Horas.`
 if (hora >= 0 && hora < 12) {
     //bom dia
     img.src = 'fotomanha.png'
-    document.body.style.backgroundImage = 'fundomanha.png'
+    
 } else if (hora >= 12 && hora < 18) {
     //boa tarde
     img.src = 'fototarde.png'
-    document.body.style.background.img('fundomanha.jpg')
+    document.body.style.background.img.src = 'fotomanha.png'
 } else {
     //boa noite
     img.src = 'fotonoite.png'
